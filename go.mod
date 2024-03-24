d0563ea87d28cd007513c1890e94a7b802950489
thingify-admin <thingify@thingify.app>
2024-03-24 18:54:19 +1030

    Integrate thing-rtc to send/receive data channel messages as IP packets over the network interface. Includes a CLI for pairing/connect operation.

--- go.mod
+++ go.mod
@@ -3,8 +3,44 @@ module thingify.app/thingify-net
 go 1.19
 
 require (
-	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8 // indirect
-	github.com/vishvananda/netlink v1.1.0 // indirect
-	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+	github.com/thingify-app/thing-rtc-go v0.0.0-20240324075316-5c5370ddde81
+	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/pion/datachannel v1.5.5 // indirect
+	github.com/pion/dtls/v2 v2.2.10 // indirect
+	github.com/pion/ice/v2 v2.3.14 // indirect
+	github.com/pion/interceptor v0.1.25 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.12 // indirect
+	github.com/pion/mediadevices v0.6.1 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.14 // indirect
+	github.com/pion/rtp v1.8.4 // indirect
+	github.com/pion/sctp v1.8.13 // indirect
+	github.com/pion/sdp/v3 v3.0.8 // indirect
+	github.com/pion/srtp/v2 v2.0.18 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport v0.14.1 // indirect
+	github.com/pion/transport/v2 v2.2.4 // indirect
+	github.com/pion/turn/v2 v2.1.5 // indirect
+	github.com/pion/udp v0.1.4 // indirect
+	github.com/pion/webrtc/v3 v3.2.29 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,50 +1,173 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io"
+	"os"
+	"path"
 
 	"github.com/songgao/water"
+	thingrtc "github.com/thingify-app/thing-rtc-go"
+	"github.com/thingify-app/thing-rtc-go/pairing"
+	"github.com/urfave/cli/v2"
 	"github.com/vishvananda/netlink"
 )
 
-func main() {
+const PAIRING_SERVER_URL = "https://thingify.deno.dev/pairing"
+const SIGNALLING_SERVER_URL = "wss://thingify.deno.dev/signalling"
+const DEFAULT_ADDRESS_RANGE = "10.0.1.0/24"
+
+func setupTunInterface(name string) (io.ReadWriteCloser, error) {
 	tun, err := water.New(water.Config{
 		DeviceType: water.TUN,
 		PlatformSpecificParams: water.PlatformSpecificParams{
-			Name: "thingify0",
+			Name: name,
 		},
 	})
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	link, err := netlink.LinkByName(tun.Name())
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	addr, err := netlink.ParseAddr("10.0.1.0/24")
+	addr, err := netlink.ParseAddr(DEFAULT_ADDRESS_RANGE)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = netlink.AddrAdd(link, addr)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = netlink.LinkSetUp(link)
 	if err != nil {
+		return nil, err
+	}
+
+	return tun, nil
+}
+
+func createPairing() pairing.Pairing {
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
 		panic(err)
 	}
 
-	buffer := make([]byte, 2000)
+	// Create our config dir if it doesn't exist.
+	configDir := path.Join(userConfigDir, "thingrtc")
+	err = os.MkdirAll(configDir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	return pairing.NewPairing(PAIRING_SERVER_URL, path.Join(configDir, "pairing.json"))
+}
+
+func respondToPairing(shortcode string) error {
+	pairing := createPairing()
+	pairing.ClearAllPairings()
+
+	fmt.Printf("Responding to pairing...\n")
+	result, err := pairing.RespondToPairing(shortcode)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Pairing succeeded, pairingId: %v\n", result.PairingId)
+	return nil
+}
+
+func listenOnTun(peer thingrtc.Peer) error {
+	tun, err := setupTunInterface("thingify0")
+	if err != nil {
+		return err
+	}
+
+	peer.OnBinaryMessage(func(message []byte) {
+		tun.Write(message)
+	})
+
 	for {
+		buffer := make([]byte, 2000)
 		n, err := tun.Read(buffer)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		log.Printf("Packet received: % x\n", buffer[:n])
+
+		peer.SendBinaryMessage(buffer[:n])
+	}
+}
+
+func connect() error {
+	pairing := createPairing()
+	pairingIds := pairing.GetAllPairingIds()
+	if len(pairingIds) == 0 {
+		return fmt.Errorf("pairing not setup, re-run pairing")
+	}
+	pairingId := pairingIds[0]
+
+	peer := thingrtc.NewPeer(SIGNALLING_SERVER_URL)
+
+	peer.OnConnectionStateChange(func(connectionState int) {
+		switch connectionState {
+		case thingrtc.Disconnected:
+			fmt.Println("Disconnected")
+		case thingrtc.Connecting:
+			fmt.Println("Connecting...")
+		case thingrtc.Connected:
+			fmt.Println("Connected.")
+		}
+	})
+
+	tokenGenerator, err := pairing.GetTokenGenerator(pairingId)
+	if err != nil {
+		return err
+	}
+
+	peer.Connect(tokenGenerator)
+
+	err = listenOnTun(peer)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	app := cli.App{
+		Name:  "thingify-net",
+		Usage: "Create virtual networks with web browsers over WebRTC.",
+		Commands: []*cli.Command{
+			{
+				Name:  "pair",
+				Usage: "Manage pairing with a peer",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "shortcode",
+						Usage:    "shortcode provided by initiating peer",
+						Required: true,
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					return respondToPairing(ctx.String("shortcode"))
+				},
+			},
+			{
+				Name:  "connect",
+				Usage: "Create an network interface to a peer",
+				Action: func(ctx *cli.Context) error {
+					return connect()
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
 	}
 }
