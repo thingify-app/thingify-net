79a134d0ef39a149e4d23a372521c71fc8221e01
thingify-admin <thingify@thingify.app>
2024-03-31 20:34:14 +1030

    Use thingrtc-peer library to pair and connect to the remote peer from the SSH example.

--- examples/ssh/index.ts
+++ examples/ssh/index.ts
@@ -1,3 +1,4 @@
+import { Pairing, PairingServer, ThingPeer } from 'thingrtc-peer';
 import { Terminal } from 'xterm';
 
 export async function runWasm() {
@@ -14,4 +15,58 @@ term.onData(str => {
     console.log(str);
 });
 
-runWasm();
+globalThis.writeToConsole = (str: string) => {
+    term.write(str);
+}
+
+const PAIRING_SERVER_URL = 'https://thingify.deno.dev/pairing';
+const SIGNALLING_SERVER_URL = 'wss://thingify.deno.dev/signalling';
+const pairingServer = new PairingServer(PAIRING_SERVER_URL);
+const pairing = new Pairing(pairingServer);
+
+const peer = new ThingPeer(SIGNALLING_SERVER_URL);
+peer.on('connectionStateChanged', state => {
+    console.log(`Peer connection state: ${state}`);
+});
+peer.on('binaryMessage', message => {
+    console.log('Binary message received:');
+    console.log(message);
+});
+
+async function tryPairing() {
+    const pendingPairing = await pairing.initiatePairing();
+    const shortcode = pendingPairing.shortcode;
+    console.log(`Initiating pairing, shortcode: ${shortcode}`);
+    await pendingPairing.redemptionResult();
+}
+
+async function connect() {
+    let pairingIds: string[] = [];
+    while (true) {
+        pairingIds = await pairing.getAllPairingIds();
+        if (pairingIds.length == 0) {
+            await tryPairing();
+        } else {
+            break;
+        }
+    }
+
+    const tokenGenerator = await pairing.getTokenGenerator(pairingIds[0]);
+    peer.connect(tokenGenerator, []);
+}
+
+async function clearPairings() {
+    peer.disconnect();
+    await pairing.clearAllPairings();
+    await connect();
+}
+
+async function main() {
+    document.getElementById('clearPairings').addEventListener('click', () => {
+        clearPairings();
+    });
+    await connect();
+    await runWasm();
+}
+
+main();
